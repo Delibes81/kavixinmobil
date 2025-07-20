44f5cf6dd1f7a1663b7165d3cc3bba03ef6eecfd
Delibes81 <164214236+Delibes81@users.noreply.github.com>
2025-07-20 03:08:30 -0600

    Fix react-map-gl version and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react', 'react-map-gl'],
+    exclude: ['lucide-react'],
     include: ['react', 'react-dom', 'react-router-dom']
   },
   build: {
