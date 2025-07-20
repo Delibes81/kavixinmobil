980620ff89507db597865730f9676d3fea04c893
Delibes81 <164214236+Delibes81@users.noreply.github.com>
2025-07-20 03:04:22 -0600

    Remove mapbox-gl from optimizeDeps exclude

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react', 'mapbox-gl'],
+    exclude: ['lucide-react', 'react-map-gl'],
     include: ['react', 'react-dom', 'react-router-dom']
   },
   build: {
